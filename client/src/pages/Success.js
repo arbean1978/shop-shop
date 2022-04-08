2e2dcb0626a8b5464f77f8481ce0dccccbab65da
Aaron Bean <arbean1978@gmail.com>
2022-04-08 15:51:25 -0600

    updatedpageswithredux

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Jumbotron';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import React, { useEffect } from "react";
+import { useMutation } from '@apollo/react-hooks';
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -10,17 +10,17 @@ function Success() {
   useEffect(() => {
     async function saveOrder() {
       const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
-
+      const products = cart.map(item => item._id);
+      
       if (products.length) {
         const { data } = await addOrder({ variables: { products } });
         const productData = data.addOrder.products;
-
+    
         productData.forEach((item) => {
           idbPromise('cart', 'delete', item);
         });
       }
-
+        
       setTimeout(() => {
         window.location.assign('/');
       }, 3000);
@@ -38,6 +38,6 @@ function Success() {
       </Jumbotron>
     </div>
   );
-}
+};
 
 export default Success;
