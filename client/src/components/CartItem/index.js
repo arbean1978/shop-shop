25bc0c37dad1cd6bacd26a5a660df4b481971422
Aaron Bean <arbean1978@gmail.com>
2022-04-08 16:03:15 -0600

    componentsredux

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,40 +1,47 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+//import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
 
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
+    const state = useSelector((state) => {
+      return state
     });
-    idbPromise('cart', 'delete', { ...item });
+    const dispatch = useDispatch();
 
-  };
-
-  const onChange = (e) => {
-    const value = e.target.value;
-    if (value === '0') {
+    const removeFromCart = item => {
       dispatch({
         type: REMOVE_FROM_CART,
         _id: item._id
       });
       idbPromise('cart', 'delete', { ...item });
+    };
+    
+    const onChange = (e) => {
+      const value = e.target.value;
+    
+      if (value === '0') {
+        dispatch({
+          type: REMOVE_FROM_CART,
+          _id: item._id
+        });
+      
+        idbPromise('cart', 'delete', { ...item });
+      } else {
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: item._id,
+          purchaseQuantity: parseInt(value)
+        });
+      
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+      }
 
-    } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+    };
 
-    }
-  }
 
   return (
     <div className="flex-row">
